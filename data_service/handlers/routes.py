8b4246a854b53f0db436ef4acb2be144a57df9ed
justice ndou <mobiusndou@gmail.com>
2021-04-15 15:28:40 +0200

    updated error handlers
    with correct error codes

--- data_service/handlers/routes.py
+++ data_service/handlers/routes.py
@@ -14,29 +14,39 @@ def warmup():
 
 @default_handlers_bp.app_errorhandler(BadRequest)
 def handle_bad_request(e: BadRequest) -> tuple:
-    return jsonify({'status': False, 'message': 'Bad Request'}), 500
+    return jsonify({'status': False, 'message': 'Bad Request'}), 400
 
 
 @default_handlers_bp.app_errorhandler(Forbidden)
 def handle_forbidden_error(e: Forbidden) -> tuple:
-    return jsonify({'status': False, 'message': 'Forbidden Request'}), 500
+    return jsonify({'status': False, 'message': 'Forbidden Request'}), 403
 
 
 @default_handlers_bp.app_errorhandler(NotFound)
 def handle_not_found_error(e: NotFound) -> tuple:
-    return jsonify({'status': False, 'message': 'Resource Not Found'}), 500
+    return jsonify({'status': False, 'message': 'Resource Not Found'}), 404
 
 
 @default_handlers_bp.app_errorhandler(MethodNotAllowed)
 def handle_method_not_allowed(e: MethodNotAllowed) -> tuple:
-    return jsonify({'status': False, 'message': 'Request Method not Allowed'}), 500
+    return jsonify({'status': False, 'message': 'Request Method not Allowed'}), 405
 
 
 @default_handlers_bp.app_errorhandler(Unauthorized)
 def handle_un_authorized_requests(e: Unauthorized) -> tuple:
-    return jsonify({'status': False, 'message': 'Request Not Authorized'}), 500
+    return jsonify({'status': False, 'message': 'Request Not Authorized'}), 401
 
 
 @default_handlers_bp.app_errorhandler(HTTPException)
 def handle_http_exception(e: HTTPException) -> tuple:
-    return jsonify({'status': False, 'message': 'HTTP Error'}), 500
+    return jsonify({'status': False, 'message': 'HTTP Error'}), 503
+
+
+"""
+# TODO- Subclass HTTP Exceptions to define my own errors
+from werkzeug.exceptions import HTTPException
+
+class PaymentRequred(HTTPException):
+    code = 402
+    description = '<p>Payment required.</p>'
+"""
