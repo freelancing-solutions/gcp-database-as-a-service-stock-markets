fc7dd7eb8850733330ee5a84d13f112f9f03709d
justice ndou <mobiusndou@gmail.com>
2021-04-30 10:34:05 +0200

    Updated Wallet View

--- data_service/views/wallet.py
+++ data_service/views/wallet.py
@@ -8,3 +8,23 @@ class WalletView:
     """
     def __init__(self):
         pass
+
+    def get_wallet(self) -> tuple:
+        pass
+
+    def create_wallet(self, wallet_data: dict) -> tuple:
+        pass
+
+    def update_wallet(self, wallet_data: dict) -> tuple:
+        pass
+
+    def reset_wallet(self, wallet_data: dict) -> tuple:
+        pass
+
+    def return_all_wallets(self) -> tuple:
+        pass
+
+    def return_wallets_by_balance(self, lower_than: int, higher_than: int) -> tuple:
+        pass
+
+    
