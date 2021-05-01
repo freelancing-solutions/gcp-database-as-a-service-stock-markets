bc05cee8b71aa3d48e390b947ae2982a6269bd2b
justice ndou <mobiusndou@gmail.com>
2021-05-01 23:30:58 +0200

    Update test_stock

--- tests/test_store/test_stock.py
+++ tests/test_store/test_stock.py
@@ -10,15 +10,25 @@ def test_stock_instance():
 
 
 def test_stock_id():
-    stock_instance.stock_id = create_id()
-    
+    sid = create_id()
+    stock_instance.stock_id = sid
+    assert stock_instance.stock_id == sid, "stock_id not set correctly"
+
 
 def test_stock_code():
-    pass
+    s_code = "ABC"
+    stock_instance.stock_code = s_code
+    assert stock_instance.stock_code == s_code, "stock code is not set correctly"
+
 
 def test_stock_name():
-    pass
+    stock_name = "ABCD"
+    stock_instance.stock_name = stock_name
+    assert stock_instance.stock_name == stock_name, "stock name is not set correctly"
+
 
 def test_stock_symbol():
-    pass
+    s_symbol = "ABC"
+    stock_instance.symbol = s_symbol
+    assert stock_instance.symbol == s_symbol,  "stock symbol is not set correctly"
 
