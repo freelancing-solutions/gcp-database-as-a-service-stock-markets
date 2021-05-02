17b8c7acde9d9c51e16b6c9bd14b7c229b58acf3
justice ndou <mobiusndou@gmail.com>
2021-05-02 12:27:59 +0200

    feat: created buy_volume_test_cases

---
+++ tests/test_store/test_buy_volume.py
@@ -0,0 +1,35 @@
+from data_service.store.stocks import BuyVolumeModel
+
+buy_volume_instance: BuyVolumeModel = BuyVolumeModel()
+
+def test_buy_volume_instance():
+    pass
+
+def test_transaction_id():
+    pass
+
+def test_stock_id():
+    pass
+
+def test_date_created():
+    pass
+
+
+def test_currency():
+    pass
+
+def test_buy_volume():
+    pass
+
+def test_buy_value():
+    pass
+
+def test_buy_ave_price():
+    pass
+
+def test_buy_market_val_percent():
+    pass
+
+def buy_trade_count():
+    pass
+
--- tests/test_store/test_stock_model.py
+++ tests/test_store/test_stock_model.py
@@ -1,18 +1,37 @@
 from data_service.store.stocks import StockModel
+from data_service.store.stocks import Stock, Broker
+from data_service.utils.utils import create_id
 
-
+stock_model_instance: StockModel = StockModel()
+stock_instance: Stock = Stock()
+broker_instance: Broker = Broker()
+# TODO add mocks for ndb.put calls
 def test_stock_model_instance():
-    pass
+    assert isinstance(stock_model_instance, StockModel), "stock model instance not instatiating correctly"
+    assert isinstance(stock_instance, Stock), "Stock not instantiating correctly"
+    assert isinstance(broker_instance, Broker), "Broker not instanting correctly"
 
 def test_exchanged_id():
-    pass
+    assert stock_model_instance.exchange_id is None, "Exchange ID Initial value invalid"
+    e_id: str = create_id()
+    stock_model_instance.exchange_id = e_id
+    assert stock_model_instance.exchange_id == e_id, "exchange id not being set correctly"
+
 
 def test_transaction_id():
-    pass
+    assert stock_model_instance.transaction_id is None, "transaction id initial value invalid"
+    t_id: str = create_id()
+    stock_model_instance.transaction_id = t_id
+    assert stock_model_instance.transaction_id == t_id, "transaction id not being set correctly"
 
 def test_stock_instance():
-    pass
+    assert stock_model_instance.stock is None, "stock model instance initial instance incorrect"
+    stock_model_instance.stock = stock_instance
+    assert isinstance(stock_model_instance.stock, Stock), "Stock model not setting stock correctly"
 
 def test_broker_instance():
-    pass
+    assert stock_model_instance.broker is None, "Broker model instance initial value invalid"
+    stock_model_instance.broker = broker_instance
+    assert isinstance(stock_model_instance.broker, Broker) , "Broker Model instance not being set correctly"
+
 
