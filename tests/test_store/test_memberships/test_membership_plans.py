e503627a4bc28585e9a732e6975035443f1570f3
justice ndou <mobiusndou@gmail.com>
2021-05-06 17:54:09 +0200

    update membership

--- tests/test_store/test_memberships/test_membership_plans.py
+++ tests/test_store/test_memberships/test_membership_plans.py
@@ -16,3 +16,12 @@ def test_plan_id():
         membership_plan_instance.plan_id = ""
 
 
+def test_plan_name():
+    plan_name: str = "bronze"
+    assert membership_plan_instance.plan_name is None, "membership_plans plan name is not being set correctly"
+    membership_plan_instance.plan_name = plan_name
+    assert membership_plan_instance.plan_name == plan_name, "membership plan_name is not being set correctly"
+    with raises(TypeError):
+        membership_plan_instance.plan_name = 0
+    with raises(ValueError):
+        membership_plan_instance.plan_name = ""
