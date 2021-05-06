bf222c35fc999dbe0482c7cc111a353d126d4498
justice ndou <mobiusndou@gmail.com>
2021-05-06 17:35:55 +0200

    updated

---
+++ tests/test_store/test_memberships/membership_plans.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from pytest import raises
+from data_service.utils.utils import create_id, datetime_now
+from tests import int_positive, int_negative
+from data_service.store.memberships import Memberships
+
+
+def test_plan_id():
+    pass
+
