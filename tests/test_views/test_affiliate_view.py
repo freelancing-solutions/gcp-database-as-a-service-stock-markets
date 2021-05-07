dd045f7529fa7ccac2744ec1ddd5bc2fc9270d66
justice ndou <mobiusndou@gmail.com>
2021-05-07 20:11:17 +0200

    update test_affiliates

---
+++ tests/test_views/test_affiliate_view.py
@@ -0,0 +1,32 @@
+from data_service.views.affiliates import AffiliatesView
+from data_service.utils.utils import create_id
+from pytest import raises
+
+def test_register_affiliate():
+    pass
+
+def test_increment_total_recruits():
+    pass
+
+def test_delete_affiliate():
+    pass
+
+def test_mark_active():
+    pass
+
+def test_get_affiliate():
+    pass
+
+def test_get_all_affiliate():
+    pass
+
+def get_active_affiliates():
+    pass
+
+def get_inactive_affiliates():
+    pass
+
+def get_deleted_affiliates():
+    pass
+
+
