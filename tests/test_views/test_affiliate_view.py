8a13f6a71e177e22547be5b554413f4e32d34b8a
mobius-crypt <mobiusndou@gmail.com>
2021-05-09 12:11:13 +0200

    update

--- tests/test_views/test_affiliate_view.py
+++ tests/test_views/test_affiliate_view.py
@@ -1,4 +1,7 @@
+import typing
 from datetime import datetime
+from random import randint
+
 from data_service.views.affiliates import AffiliatesView
 from data_service.store.affiliates import Affiliates
 from data_service.utils.utils import create_id
@@ -17,14 +20,16 @@ affiliate_data_mock: dict = {
     "is_deleted": False
 }
 
+
 class AffiliateQueryMock:
-    affiliate_instance = Affiliates()
+    affiliate_instance: Affiliates = Affiliates()
+    results_range: int = randint(0, 100)
 
     def __init__(self):
         pass
 
-    def fetch(self) -> list:
-        return [self.affiliate_instance]
+    def fetch(self) -> typing.List[Affiliates]:
+        return [self.affiliate_instance for _ in range(self.results_range)]
 
     def get(self) -> Affiliates:
         return self.affiliate_instance
@@ -32,7 +37,6 @@ class AffiliateQueryMock:
 
 # noinspection PyShadowingNames
 def test_register_affiliate(mocker):
-
     mocker.patch('google.cloud.ndb.Model.put', return_value=create_id())
     mocker.patch('google.cloud.ndb.Model.query', return_value=AffiliateQueryMock())
     mocker.patch('data_service.store.affiliates.AffiliatesValidators.user_already_registered', return_value=False)
@@ -64,6 +68,7 @@ def test_register_affiliate(mocker):
         assert response_dict.get('message') is not None, "affiliate message is not being set correctly"
     mocker.stopall()
 
+
 # noinspection PyShadowingNames
 def test_increment_decrement_total_recruits(mocker):
     mocker.patch('google.cloud.ndb.Model.put', return_value=create_id())
@@ -77,6 +82,7 @@ def test_increment_decrement_total_recruits(mocker):
         assert affiliate_dict['status'], "failing to set the return boolean status"
         assert affiliate_dict.get('message') is not None, "failed to set message"
     mocker.stopall()
+
 
 # noinspection PyShadowingNames
 def test_delete_affiliate(mocker):
@@ -93,6 +99,7 @@ def test_delete_affiliate(mocker):
         message: str = "affiliate delete operation response status was not set correctly"
         assert isinstance(affiliate_dict['status'], bool) and affiliate_dict['status'], message
     mocker.stopall()
+
 
 # noinspection PyShadowingNames
 def test_mark_active(mocker):
@@ -113,6 +120,7 @@ def test_mark_active(mocker):
         assert status == 500, "passing invalid values is not triggering errors"
     mocker.stopall()
 
+
 # noinspection PyShadowingNames
 def test_get_affiliate(mocker):
     mocker.patch('google.cloud.ndb.Model.put', return_value=create_id())
@@ -132,33 +140,71 @@ def test_get_all_affiliate(mocker):
     mocker.patch('google.cloud.ndb.Model.query', return_value=AffiliateQueryMock())
     # TODo complete the test cases
     with test_app().app_context():
-        pass
+        affiliate_instance: AffiliatesView = AffiliatesView()
+        response, status = affiliate_instance.get_all_affiliates()
+        assert status == 200, "get_all_affiliates unable to fetch affiliates"
+        response_data: dict = response.get_json()
+        assert response_data.get('payload') is not None, "get_all_affiliates payload is not set properly"
+        assert response_data.get('message') is not None, "get_all_affiliates message is not set properly"
+
     mocker.stopall()
 
+
 # noinspection PyShadowingNames,DuplicatedCode
-def get_active_affiliates(mocker):
+def test_active_affiliates(mocker):
     mocker.patch('google.cloud.ndb.Model.put', return_value=create_id())
     mocker.patch('google.cloud.ndb.Model.query', return_value=AffiliateQueryMock())
-    # TODO Add more test cases
-
     with test_app().app_context():
-        pass
+        affiliate_instance: AffiliatesView = AffiliatesView()
+        response, status = affiliate_instance.get_active_affiliates()
+        assert status == 200, "get_active_affiliates unable to fetch affiliates"
+        response_data: dict = response.get_json()
+        assert response_data.get('payload') is not None, "get_active_affiliates payload is not set properly"
+        assert response_data.get('message') is not None, "get_active_affiliates message is not set properly"
+
     mocker.stopall()
 
+
 # noinspection PyShadowingNames,DuplicatedCode
-def get_inactive_affiliates(mocker):
+def test_inactive_affiliates(mocker):
     mocker.patch('google.cloud.ndb.Model.put', return_value=create_id())
     mocker.patch('google.cloud.ndb.Model.query', return_value=AffiliateQueryMock())
 
     with test_app().app_context():
-        pass
+        affiliate_instance: AffiliatesView = AffiliatesView()
+        response, status = affiliate_instance.get_in_active_affiliates()
+        assert status == 200, "get_inactive_affiliates unable to fetch affiliates"
+        response_data: dict = response.get_json()
+        assert response_data.get('payload') is not None, "get_inactive_affiliates payload is not set properly"
+        assert response_data.get('message') is not None, "get_inactive_affiliates message is not set properly"
     mocker.stopall()
 
+
 # noinspection PyShadowingNames,DuplicatedCode
-def get_deleted_affiliates(mocker):
+def test_deleted_affiliates(mocker):
     mocker.patch('google.cloud.ndb.Model.put', return_value=create_id())
     mocker.patch('google.cloud.ndb.Model.query', return_value=AffiliateQueryMock())
 
     with test_app().app_context():
-        pass
+        affiliate_instance: AffiliatesView = AffiliatesView()
+        response, status = affiliate_instance.get_deleted_affiliates()
+        assert status == 200, "get_deleted_affiliates unable to fetch affiliates"
+        response_data: dict = response.get_json()
+        assert response_data.get('payload') is not None, "get_deleted_affiliates payload is not set properly"
+        assert response_data.get('message') is not None, "get_deleted_affiliates message is not set properly"
+    mocker.stopall()
+
+
+# noinspection PyShadowingNames,DuplicatedCode
+def test_undeleted_affiliates(mocker):
+    mocker.patch('google.cloud.ndb.Model.put', return_value=create_id())
+    mocker.patch('google.cloud.ndb.Model.query', return_value=AffiliateQueryMock())
+
+    with test_app().app_context():
+        affiliate_instance: AffiliatesView = AffiliatesView()
+        response, status = affiliate_instance.get_not_deleted_affiliates()
+        assert status == 200, "get_not_deleted_affiliates unable to fetch affiliates"
+        response_data: dict = response.get_json()
+        assert response_data.get('payload') is not None, "get_not_deleted_affiliates payload is not set properly"
+        assert response_data.get('message') is not None, "get_not_deleted_affiliates message is not set properly"
     mocker.stopall()
